29a1234daff08aca86e6f934973aa7b0a1496bef
BoengGit <edukasiasqor@gmail.com>
2022-12-08 05:01:02 +0700

    wip

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -33,4 +33,19 @@ class FrontendController extends Controller
             return redirect('/')->with('status', "Slug doesnot exists");
         }
     }
+
+    public function productview($cate_slug, $prod_slug)
+    {
+        if (Category::where('slug', $cate_slug)->exists()) {
+            if (Product::where('slug', $prod_slug)->exists()) {
+                $products = Product::where('slug', $prod_slug)->first();
+                return view('frontend.products.view', compact('products'));
+            }
+            else {
+                return redirect('/')->with('status', "The link was broken");
+            }
+        } else {
+            return redirect('/')->with('status', "No such category found");
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FrontendController::class, 'index']);
 Route::get('category', [FrontendController::class, 'category']);
 Route::get('view-category/{slug}', [FrontendController::class, 'viewcategory']);
+Route::get('category/{cate_slug}/{prod_Slug}', [FrontendController::class, 'productview']);
 
 Auth::routes();
 
