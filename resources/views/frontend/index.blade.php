2b11315fe72aebe47e17958f44df60c79e7d9e90
BoengGit <edukasiasqor@gmail.com>
2022-12-07 23:11:03 +0700

    wip

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -36,15 +36,18 @@
                 <div class="owl-carousel featured-carousel owl-theme">
                     @foreach ($trending_category as $tcategory)
                         <div class="item">
-                            <div class="card">
-                                <img src="{{ asset('assets/uploads/category/' . $tcategory->image) }}" alt="Product-image">
-                                <div class="card-body">
-                                    <h5>{{ $tcategory->name }}</h5>
-                                    <p>
-                                        {{ $tcategory->description }}
-                                    </p>
+                            <a href="{{ url('view-category/'.$tcategory->slug) }}">
+                                <div class="card">
+                                    <img src="{{ asset('assets/uploads/category/' . $tcategory->image) }}"
+                                        alt="Product-image">
+                                    <div class="card-body">
+                                        <h5>{{ $tcategory->name }}</h5>
+                                        <p>
+                                            {{ $tcategory->description }}
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     @endforeach
                 </div>
