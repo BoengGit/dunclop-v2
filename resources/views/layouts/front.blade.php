8f77ec4ebf847321041fcb6815bec881a8f65d78
BoengGit <edukasiasqor@gmail.com>
2022-12-08 05:18:45 +0700

    wip

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -21,11 +21,22 @@
     <!-- Custom Css-->
     <link href="{{ asset('frontend/css/custom.css') }}" rel="stylesheet" />
 
+    <!-- Owl Caraousel-->
     <link href="{{ asset('frontend/css/owl.carousel.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('frontend/css/owl.theme.default.min.css') }}" rel="stylesheet" />
 
+    <!-- Google Awesome-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Open+Sans:wght@600&family=Roboto&display=swap"
+        rel="stylesheet">
+
+    <!-- Font Awesome-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+
     <style>
-        a{
+        a {
             text-decoration: none !important;
         }
     </style>
@@ -37,26 +48,25 @@
 
     <!-- Begin page -->
 
-            <div class="content">
-                @yield('content')
-            </div>
+    <div class="content">
+        @yield('content')
+    </div>
 
-        <!-- END layout-wrapper -->
+    <!-- END layout-wrapper -->
 
     <!-- JAVASCRIPT -->
 
-        <script src="{{ asset('frontend/js/jquery-3.6.1.min.js') }}"></script>
-        <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/jquery-3.6.1.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
 
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-        @if (session('status'))
-            <script>
-                swal("{{ session('status') }}");
-            </script>
-        @endif
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    @if (session('status'))
+        <script>
+            swal("{{ session('status') }}");
+        </script> @endif
 
-        @yield('scripts')
+    @yield('scripts')
 </body>
 
 </html>
